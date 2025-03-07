febf8a8f13b5f103075195da87f43b4fcf4a985f
Long Nguyen <90327629+longnguyen1305@users.noreply.github.com>
2025-03-07 11:11:38 -0400

    Initialize basic css

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { ToastContainer } from 'react-toastify';
 
 import Dashboard from './components/Dashboard';
-import Login from './components/Login';
-import Register from './components/Register';
+import Login from './components/loginRegister/Login';
+import Register from './components/loginRegister/Register';
 import Project from './components/Project';
 
 function App() {
---
+++ src/components/loginRegister/Login.js
@@ -0,0 +1,87 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import styles from './index.module.css'
+
+const Login = ({ setAuth }) => {
+
+    const [inputs, setInputs] = useState({
+        email: "",
+        password: ""
+    });
+    const {email, password} = inputs;
+    const onChange = (e) => {
+        setInputs({...inputs, [e.target.name] : e.target.value});
+    }
+    const onSubmitForm = async (e) => {
+        e.preventDefault();
+
+        try {
+            const body = {email, password}
+            const response = await fetch("http://localhost:5000/auth/login", {
+                method: "POST",
+                headers: {"Content-Type" : "application/json"},
+                body: JSON.stringify(body)
+            });
+            const parseResponse = await response.json();
+
+            if (parseResponse.token) {
+                localStorage.setItem("token", parseResponse.token);
+                setAuth(true);
+                toast.success("Login successfully!");
+            } else {
+                setAuth(false);
+                toast.error(parseResponse);
+            }
+
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    return (
+        <Fragment>
+            <div className={styles.container}>
+                <header>
+                    <h1 className={styles.logo}>LOGO</h1>
+                </header>
+                <div className={styles.wrapper}>
+                    <div className={styles.formbox}>
+                        <h1>Login</h1>
+                        <form onSubmit={onSubmitForm}>
+                            <div className={styles.inputbox}>
+                                <span className={styles.icon}><ion-icon name="mail-sharp"></ion-icon></span>
+                                <input
+                                    type='email'
+                                    name='email'
+                                    value={email}
+                                    onChange={onChange}
+                                    required
+                                />
+                                <label>Email</label>
+                            </div>
+                            <div className={styles.inputbox}>
+                                <span className={styles.icon}><ion-icon name="lock-closed-sharp"></ion-icon></span>
+                                <input
+                                    type='password'
+                                    name='password'
+                                    value={password}
+                                    onChange={onChange}
+                                    required
+                                />
+                                <label>Password</label>
+                            </div>
+                            <button className={styles.btn}>Login</button>
+                            <div className={styles.loginregister}>
+                                <p>Don't have an account? <Link to="/register" className={styles.link}>Register</Link></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+        </Fragment>
+    );
+}
+
+export default Login;
